a1b87d51862315e67ac44d9171542ce318c3f719
Wesley Spikes <wesley.spikes@gmail.com>
2014-04-14 03:13:54 -0700

    Add a really primitive CLI entrypoint

--- pyfuck/cli.py
+++ pyfuck/cli.py
@@ -1,9 +1,24 @@
 """ CLI code for pyfuck """
 
+# To silence the linter.
+from __future__ import print_function
+
+import sys
+from pyfuck import PyFucker
+
 
 def main():
-    """ Do stuff here. """
-    pass
+    """ For now, we're just adding a really trivial runner. """
+    if len(sys.argv) != 2:
+        print("Usage: %s <script>\n" % sys.argv[0])
+        sys.exit(1)
+
+    script = sys.argv[1]
+    with open(script, 'r') as fh_script:
+        code = fh_script.read()
+
+    inst = PyFucker(code)
+    inst.run()
 
 if __name__ == '__main__':
     main()
